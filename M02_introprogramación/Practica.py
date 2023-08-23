86e7a897aecb21ec68eb4584ae8f882b7077a6a9
Cris Zamorano <61209933+HX-CZamorano2@users.noreply.github.com>
2023-08-23 07:52:13 -0300

    Actualización Readme

--- M02_introprogramación/Practica.py
+++ M02_introprogramación/Practica.py
@@ -3,8 +3,8 @@
 
 # A partir del próximo módulo: M03_variablesydatos vas a tener actividades para realizar.
 
-print('Hola Mundo!')
-a = 20
+print('Hola Mundo! o en ingles ¡Hello World')
+a = 5
 while a > 0:
     print(a)
     a -= 1
